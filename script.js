b95a9f8c6341a4041520bb0ea398b411ac5d6de1
Onur Temiz <onurmain@gmail.com>
2019-05-04 18:55:49 +0300

    Add Size - Random Color

--- script.js
+++ script.js
@@ -1,33 +1,80 @@
-let gridBig = 4;
-let gridSize = 100;
+let gridSize = 4;
 
-const container = document.getElementById("container");
-container.style.gridTemplate = `repeat(${gridSize}, ${gridBig}px) / repeat(${gridSize}, ${gridBig}px)`;
+const HEIGHT = 400;
+const WIDTH = 400;
 
-for (let i = 1; i < gridSize + 1; i++) {
-  for (let j = 1; j < gridSize + 1; j++) {
-    const grid = document.createElement("div");
-    grid.className = "grid";
-    grid.id = `grid-${i}-${j}`;
-    grid.style.gridArea = `${i}/${j}/span 1/span 1`;
-    grid.style.width = `${gridBig}px`;
-    grid.style.height = `${gridBig}px`;
-    container.appendChild(grid);
+function createGrid() {
+  const container = document.getElementById("container");
+  container.style.gridTemplate = `repeat(${Math.floor(
+    HEIGHT / gridSize
+  )}, ${gridSize}px) / repeat(${Math.floor(WIDTH / gridSize)}, ${gridSize}px)`;
+  for (let i = 1; i < Math.floor(HEIGHT / gridSize) + 1; i++) {
+    for (let j = 1; j < Math.floor(WIDTH / gridSize) + 1; j++) {
+      const grid = document.createElement("div");
+      grid.className = "grid";
+      grid.id = `grid-${i}-${j}`;
+      grid.style.gridArea = `${i}/${j}/span 1/span 1`;
+      grid.style.width = `${gridSize}px`;
+      grid.style.height = `${gridSize}px`;
+      container.appendChild(grid);
+    }
+  }
+  function changeBackground() {
+    this.style.backgroundColor = currentColor;
+  }
+
+  const grid = document.querySelectorAll(".grid");
+  grid.forEach(grid => grid.addEventListener("mouseover", changeBackground));
+
+  const clearButton = document.querySelector("#clear");
+  clearButton.addEventListener("click", clearBoard);
+
+  function clearBoard() {
+    grid.forEach(grid => (grid.style.backgroundColor = "black"));
   }
 }
 
-function changeBackground() {
-  this.style.backgroundColor = "red";
+let currentColor;
+randomColor();
+
+function randomColor() {
+  currentColor = `rgb(${randNumber(1, 255)},${randNumber(1, 255)},${randNumber(
+    1,
+    255
+  )})`;
 }
 
-const grid = document.querySelectorAll(".grid");
+const colorButton = document.querySelector("#color");
+colorButton.addEventListener("click", randomColor);
 
-grid.forEach(grid => grid.addEventListener("mouseover", changeBackground));
+function deleteGrid() {
+  const body = document.querySelector("body");
+  const container = document.getElementById("container");
+  body.removeChild(container);
+  const newContainer = document.createElement("div");
+  newContainer.id = "container";
+  body.appendChild(newContainer);
+}
 
-const clearButton = document.querySelector("button");
+createGrid();
 
-clearButton.addEventListener("click", clearBoard);
+const sizeButton = document.querySelector("#size");
+sizeButton.addEventListener("click", changeSizePropmt);
 
-function clearBoard() {
-  grid.forEach(grid => (grid.style.backgroundColor = "black"));
+function changeSizePropmt() {
+  let sizeInput = window.prompt("1-400 Arası Sayı Girin", 10);
+  while (sizeInput != null || sizeInput != "") {
+    if (!isNaN(sizeInput)) {
+      break;
+    } else {
+      sizeInput = window.prompt("Lütfen Sadece 1-400 Arası Sayı Girin");
+    }
+  }
+  deleteGrid();
+  gridSize = Number(sizeInput);
+  createGrid();
+}
+
+function randNumber(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
 }
